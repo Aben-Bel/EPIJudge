3836a33691c60080e423219d71f4737a47fc2c19
aben-bel <abenezer.sbelay@gmail.com>
2022-05-19 13:39:23 +0300

    Solve parity problem

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -3,7 +3,11 @@ from test_framework import generic_test
 
 def parity(x: int) -> int:
     # TODO - you fill in here.
-    return 0
+    count = 0
+    while x:
+        count += x&1
+        x>>=1
+    return count%2
 
 
 if __name__ == '__main__':
--- epi_judge_python/swap_bits.py
+++ epi_judge_python/swap_bits.py
@@ -3,7 +3,7 @@ from test_framework import generic_test
 
 def swap_bits(x, i, j):
     # TODO - you fill in here.
-    return 0
+    lsb = 
 
 
 if __name__ == '__main__':
