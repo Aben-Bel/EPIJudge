9031a6629d6c46c24af5502dd34cad680b73da2d
aben-bel <abenezer.sbelay@gmail.com>
2022-06-13 11:04:29 +0300

    solve buy and sell stock

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -4,8 +4,14 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_once(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    res = 0
+    # 31.0,31.5,275,295,260,270,290,230,255,250
+    cur_min = prices[0]
+    for stock in prices:
+        if cur_min > stock:
+            cur_min = stock
+        res = max(res, stock - cur_min)
+    return res
 
 
 if __name__ == '__main__':
--- epi_judge_python/buy_and_sell_stock_twice.py
+++ epi_judge_python/buy_and_sell_stock_twice.py
@@ -1,11 +1,32 @@
+from functools import lru_cache
+from turtle import back
 from typing import List
 
 from test_framework import generic_test
 
 
+
+
 def buy_and_sell_stock_twice(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    forward = [0]*(len(prices)+1)
+    runningMinimum = float('inf')
+    for i in range(1,len(prices)+1):
+        if runningMinimum > prices[i-1]:
+            runningMinimum = prices[i-1]
+        forward[i] = max(prices[i-1] - runningMinimum, forward[i-1])
+    backward = [0]*(len(prices)+1)
+    
+    runningMaximum = float('-inf')
+    for i in range(len(prices)-1, -1, -1):
+        if runningMaximum < prices[i]:
+            runningMaximum = prices[i]
+        backward[i] = max(runningMaximum - prices[i], backward[i+1])
+    backward = backward[:len(backward)-1]
+    forward = forward[1:] + [0]
+    maxProfit = float('-inf')
+    for i in range(len(forward)-1):
+        maxProfit = max(maxProfit, forward[i-1]+backward[i])
+    return maxProfit
 
 
 if __name__ == '__main__':
