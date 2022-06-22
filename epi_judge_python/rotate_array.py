80ed71666a1dd8df8b73e78e4712d74f8c5bc61a
aben-bel <abenezer.sbelay@gmail.com>
2022-06-22 08:59:43 +0300

    Solve string_integer interconversion

--- epi_judge_python/rotate_array.py
+++ epi_judge_python/rotate_array.py
@@ -6,8 +6,22 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def rotate_array(rotate_amount: int, A: List[int]) -> None:
-    # TODO - you fill in here.
-    return
+    rotate_amount = rotate_amount % 4
+    n = len(A)
+    result = [[0 for i in range(n)] for j in range(n)]
+    # print("Result: ", result)
+
+    while rotate_amount > 0:
+        for i in range(n):
+            for j in range(n):
+                result[j][n-i-1] = A[i][j]
+        # print("Rotated: ", result)
+        for i in range(n):
+            for j in range(n):
+                A[i][j] = result[i][j]
+        # print("Copied to A: ", A)
+        rotate_amount -= 1
+
 
 
 @enable_executor_hook
--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -3,15 +3,35 @@ from test_framework.test_failure import TestFailure
 
 
 def int_to_string(x: int) -> str:
-    # TODO - you fill in here.
-    return '0'
+    sign = 1
+    if x < 0:
+        sign = -1
+        x = x*(-1)
+    res = ""
+    while x > 0:
+        res = str(x%10) + res
+        x//=10
+    if res == "":
+        res = "0"
+    return res if sign == 1 else "-"+res
 
 
 def string_to_int(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    sign = 1
+    if s[0] == "-":
+        sign = -1
+        s = s[1:]
+    if s[0] == "+":
+        s = s[1:]
+    res = 0
+    # 123 
+    for char in s:
+        res = res + int(char) 
+        res *= 10
+    
+    return (res//10)*sign
 
-
+# print("ans:", string_to_int('0'))
 def wrapper(x, s):
     if int(int_to_string(x)) != x:
         raise TestFailure('Int to string conversion failed')
