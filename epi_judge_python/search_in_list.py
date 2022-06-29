c8ae90cc85d2c803895476c06aeb78fa97d9ec5c
aben-bel <abenezer.sbelay@gmail.com>
2022-06-29 16:29:29 +0300

    implement linked list helper functions

--- epi_judge_python/search_in_list.py
+++ epi_judge_python/search_in_list.py
@@ -3,9 +3,11 @@ from test_framework import generic_test
 
 
 def search_list(L: ListNode, key: int) -> ListNode:
-    # TODO - you fill in here.
-    return ListNode()
-
+    while L:
+        if L.data == key:
+            return L
+        L = L.next
+    return L
 
 def search_list_wrapper(L, key):
     result = search_list(L, key)
